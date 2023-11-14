c8a4a917a4b0f6dd65ceb7b99659c4ed3174cb56
Flopiras <Flopiras@yahoo.it>
2023-11-14 14:28:59 +0100

    added Cuffie class

---
+++ src/org/lessons/java/shop/Cuffie.java
@@ -0,0 +1,30 @@
+package org.lessons.java.shop;
+
+public class Cuffie  extends Prodotto{
+	
+	private String colore;
+	private String tipo;
+	
+	//costruttore
+	public Cuffie(String nome, String descrizione, int prezzo, int iva, String colore, String tipo) {
+		super(nome,descrizione, prezzo, iva);
+	}
+
+	//getters and setters
+	public String getColore() {
+		return colore;
+	}
+
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
